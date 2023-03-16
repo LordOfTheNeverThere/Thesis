e7d912384262c92eee4d277a91c9dd507a26d4ea
Miguel Dinis de Sousa <miguel.dinis.s@tecnico.ulisboa.pt>
2023-03-16 15:05:42 +0000

    Pre-recall Curve, added Corum TrueY

--- src/baseModel.py
+++ src/baseModel.py
@@ -1,41 +1,50 @@
 # %% Imports
 import pandas as pd
 import seaborn as sb
-import numpy as np
-import matplotlib.pyplot as plt
+# import numpy as np
+# import matplotlib.pyplot as plt
 import utils
 
 
 # %% Load Dataset
 
-proteinsData = pd.read_csv('proteomicData.csv', index_col='modelId')
+proteinsData = pd.read_csv(
+    '../data/datasetsTese/proteomicsDataTrans.csv', index_col='modelID')
 
 
-# %% Trim External Dataset and our dataset afterwards (watches out for deprecated id's)
-
-trimmedProteinsData, trimmedExternalData = utils.trimDatasets() 
 # %% Calculate Pearson Coorelation Value
 
 # pearsonCorrRaw = proteinsData.corr(method='pearson')
-# pearsonCorrRaw.to_csv('pearsonCorrRaw.csv', index=False)
-pearsonCorrRaw = pd.read_csv('../data/datasetsTese/pearsonCorrRaw.csv')
+# pearsonCorrRaw.to_csv('../data/datasetsTese/pearsonCorrRaw.csv', index=False)
+# pearsonCorrRaw = pd.read_csv('../data/datasetsTese/pearsonCorrRaw.csv')
 # %% Get upwards triangular matrix data
 
-pairwiseCorrRawSeries = utils.getPairwiseCorrData(data=pearsonCorrRaw)
-pairwiseCorrRawSeries.sort_values(ascending=False, inplace=True) # Order it (descending Order)
-pairwiseCorrRawSeries.to_csv('pairwiseCorrRawSeries.csv')
-pairwiseCorrRawSeries =  pd.read_csv('pairwiseCorrRawSeries.csv', index_col='Unnamed: 0')
+# pairwiseCorrRawSeries = utils.getPairwiseCorrData(data=pearsonCorrRaw)
+# pairwiseCorrRawSeries.sort_values(ascending=False, inplace=True) # Order it (descending Order)
+# pairwiseCorrRawSeries.to_csv('../data/datasetsTese/pairwiseCorrRawSeries.csv')
+# pairwiseCorrRawSeries = pd.read_csv(
+#     '../data/datasetsTese/pairwiseCorrRawSeries.csv', index_col='Unnamed: 0')
 
 #%% Create columns for better integration with other external Databases by using the entrez ID of the subunits
 
-pairwiseCorrRawData = utils.getGeneIDsCol(pairwiseCorrRawSeries)
-
+# pairwiseCorrRawData = utils.getGeneIDsCol(pairwiseCorrRawSeries)
+pairwiseCorrRawData = pd.read_csv(
+    '../data/datasetsTese/BaseModelPairwise.csv', index_col='Unnamed: 0')
 
 
 # %% Load external Datasets
 
-corumPPI = pd.read_json('../externalDatasets/corumPPI.json')
-stringPPI = pd.read_table('../externalDatasets/stringPPI.txt', sep=' ')
-geneIds = pd.read_table('../externalDatasets/geneIDsNCBI.tsv')
+# corumPPI = pd.read_json('../data/externalDatasets/corumPPI.json')
+# stringPPI = pd.read_table('../data/externalDatasets/stringPPI.txt', sep=' ')
+
+# %% Dummy Test Pipeline function
+_, _ = utils.getPairwiseCorrelation(fileName='Dummy', data=proteinsData.head(3), columnName='Test')
+
+# %% Get get true or false on pairwise correlation
+listOfSets = utils.getCorumListOfInteractions()
+utils.addGroundTruth(listOfSets, pairwiseCorrRawData, 'Corum')
+
+
+# %% Create Recall Curves
 
 # %%
--- src/utils.py
+++ src/utils.py
@@ -4,8 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def getPairwiseCorrData(data: pd.DataFrame) -> pd.Series:
+def getPairwiseCorrData(data: pd.DataFrame, columnName='correlation') -> pd.Series:
 
+    data = data.copy()
     row = 0
     col = 1
     columns = data.columns  # list of Columns/Proteins
@@ -26,7 +27,8 @@ def getPairwiseCorrData(data: pd.DataFrame) -> pd.Series:
             col += 1
             row = 0
 
-    pairwiseCorrsSeries = pd.Series(data=pairwiseCorrs, index=proteinIds)
+    pairwiseCorrsSeries = pd.Series(
+        data=pairwiseCorrs, index=proteinIds, name=columnName)
     return pairwiseCorrsSeries
 
 
@@ -43,30 +45,106 @@ def getPairwiseCorrData(data: pd.DataFrame) -> pd.Series:
 
 def getGeneIDsCol(data: pd.DataFrame) -> pd.DataFrame:
 
+    data = data.copy()
     proteinSubunitsList = [proteinComplex.split('-+-')
                            for proteinComplex in list(data.index)]
-    
-    proteinAList = proteinBList = proteinABList = []
-    
+
+    proteinAList = []
+    proteinBList = []
+
     for proteinSubunits in proteinSubunitsList:
 
         # proteinA = fetchGeneEntrezID(proteinSubunits[0]) # Convert string to Entrez Gene ID
         # proteinB = fetchGeneEntrezID(proteinSubunits[1])
         # No conversion need as of now we are working with gene symbols
 
-        proteinA = proteinSubunits[0]; proteinB = proteinSubunits[1]
+        proteinA = proteinSubunits[0]
+        proteinB = proteinSubunits[1]
 
         proteinAList.append(proteinA)
         proteinBList.append(proteinB)
-        proteinABList.append(proteinA + ',' + proteinB)
+
+    # Assigning new cols with the subunit information
+    data['proteinA'] = proteinAList
+    data['proteinB'] = proteinBList
+
+    return data
+
+
+def getPairwiseCorrelation(data: pd.DataFrame, fileName: str, columnName: str) -> tuple:
+    """_summary_
+
+    Args:
+        data (pd.DataFrame): Proteomics Data in transposed form
+        fileName (str): Name of the csv file created with the pairwise correlation
+
+    Returns:
+        tuple: Tuple with Dataframes with the pairwise correlation, the first one with subuints of the protein protein complex
+    """
+    data = data.copy()
+
+    pearsonCorrMatrix = data.corr(method='pearson')
+    pairwiseCorrData = getPairwiseCorrData(
+        data=pearsonCorrMatrix, columnName=columnName)
+    pairwiseCorrData.sort_values(ascending=False, inplace=True)
+    proteinIDCorrData = getGeneIDsCol(data=pairwiseCorrData)
+
+    proteinIDCorrData.to_csv(
+        '../data/datasetsTese/' + fileName + '.csv')
+
+    return proteinIDCorrData, pairwiseCorrData
+
+
+def addGroundTruth(listOfsets: list, data: pd.DataFrame, externalDatasetName: str):
     
-    #Assigning new cols with the subunit information
-    data['proteinA'] = proteinAList; data['proteinB'] = proteinBList; data['proteinAB'] = proteinABList
+    data = data.copy()
+    data[externalDatasetName] = None
 
-    return data 
+    def addExternalTrueY(model):
 
+        found = 0
+        index = 0
 
+        while not found and index < len(listOfsets):
+
+            subset = listOfsets[index]
+            if (model['proteinA'] in subset) and (model['proteinB'] in subset):
+                found = 1
+                
+                # outputList = outputList.append(1)  # There is interaction
+
+            index += 1
+            
+        model[externalDatasetName] = found
+
+        return model[externalDatasetName]
+
+    data[externalDatasetName] = data.apply(
+        axis=1, func= lambda model: addExternalTrueY(model))
     
-def trimDatasets(internalData, internalDataCol, externalData, externalDataCol):
+    return data
 
-    
+def getCorumListOfInteractions():
+    def joinGeneNames(interaction):
+        subset1 = interaction['subunits(Gene name)']
+        subset2 = interaction['subunits(Gene name syn)']
+        subset1 = subset1.split(';')
+        subset2 = subset2.split(';')
+        lenghtSubset1 = len(subset1)
+
+        for index in range(0, lenghtSubset1):
+            proteinAliases = subset2[index]
+            proteinAliases = proteinAliases.split(' ')
+            subset1 = subset1 + proteinAliases
+
+        subset1 = set(subset1)
+        subset1.discard('None')
+        subset1.discard('')
+
+        return subset1
+
+
+    corumPPI = pd.read_json('../data/externalDatasets/corumPPI.json')
+    listOfSets = corumPPI.apply(axis=1, func=lambda interaction: joinGeneNames(interaction))
+
+    return listOfSets
