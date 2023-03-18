d668dd5d47f7d66b39fff677a5a32ceb97351df6
Miguel Dinis de Sousa <miguel.dinis.s@tecnico.ulisboa.pt>
2023-03-18 11:41:09 +0000

    Readded Maven Project

--- java_code/src/main/java/pt/thesis/App.java
+++ java_code/src/main/java/pt/thesis/App.java
@@ -6,8 +6,7 @@ public class App
 {
     public static void main( String[] args )
     {
-    
-        Table t = Table.read().csv(CsvReadOptions.builder("../data/datasetsTese/BaseModelPairwise.csv"));
+        Table t = Table.read().csv(CsvReadOptions.builder("data/datasetsTese/BaseModelPairwise.csv"));
         System.out.println(t.print());
     }
 }
