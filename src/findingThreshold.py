be07d2d857ea49ee4a4ccbe8358aec9f04ecaec9
TheLordOfTheNeverThere <miguel.dinis.s@tecnico.ulisboa.pt>
2023-03-30 20:31:38 +0100

    changes after meeting

--- src/findingThreshold.py
+++ src/findingThreshold.py
@@ -5,74 +5,79 @@ from sklearn.metrics import auc
 import matplotlib.pyplot as plt
 import utils
 from sklearn.metrics import auc
+from classes import ppiDataset
 
 
 PATH = "../data"
+RANDOMSTATE = 42
 
-# Load Dataset
+# proteinsData = pd.read_csv(PATH+'/datasetsTese/proteomicsDataTrans.csv', index_col='modelID')
 
-proteinsData = pd.read_csv(
-    PATH + '/datasetsTese/proteomicsDataTrans.csv', index_col='modelID')
+# pairwiseCorrData = pd.read_csv(PATH+'/datasetsTese/BaseModelPairwise.csv', index_col='PPI')
+
+# What Professor Pedro did not ask For :( 
+
+# def getAUCvsThresholdPlot(pairwiseCorrData:pd.DataFrame) -> None:
+
+#     # find the best count threshold n
+#     best = True
+#     previousAUC = 0
+#     threshold = 2
+#     aucList=[]
+#     thresholdList =[]
+#     while threshold < 101:
+        
+#         queriedPairCorrData = pairwiseCorrData.query('counts > @threshold')
+#         corrCumSum = np.cumsum(queriedPairCorrData['Corum']) / np.sum(queriedPairCorrData['Corum'])
+#         indexes = np.array(queriedPairCorrData.reset_index().index) / queriedPairCorrData.shape[0]
+#         currentAUC = auc(indexes, corrCumSum)
+#         #Update Lists for Curve plot
+#         aucList.append(currentAUC)
+#         thresholdList.append(threshold)
+        
+        
+#         if best and currentAUC < previousAUC:
+#             best = False
+#             print('The limit threshold of interactions were we start to lose information after increasing it is: ' + str(threshold-1) + '\n with an auc of ' + str(previousAUC))
+#         # print(str(currentAUC) + '>=' + str(previousAUC) + '\n' + str(best))
+#         threshold += 1
+#         previousAUC = currentAUC
 
 
+#     # make chart with auc per n threshold
 
-# Get Pairwise Correlation with n
-pairwiseCorrData = utils.getPairwiseCorrelation(
-    fileName='BaseModelPairwise', data=proteinsData, columnName='globalCorrelation')
+#     _, ax = plt.subplots(1, 1, figsize=(4, 3), dpi=600)
+#     ax.plot(
+#         thresholdList,
+#         aucList,
+#         c='green',
+#     )
+#     ax.plot([0,100],[0.78,0.78], "r--", label='AUC == 0.78',lw=0.7)
+#     ax.plot([threshold-1,threshold-1],[0,1], "b--", label='max n==' + str(threshold-1),lw=0.6)
 
-# Load external Datasets
-corumPPI = pd.read_json(PATH + '/externalDatasets/corumPPI.json')
+#     ax.legend(loc="lower right", frameon=False)
+#     ax.set_ybound(lower=0.6, upper=0.9)
+#     ax.set_ylabel("Area Under the Curve")
+#     ax.set_xlabel("Minimum interaction threshold")
+#     ax.set_title('AUC vs sample frequency of PPI threshold')
+#     ax.grid(True, axis="both", ls="-", lw=0.1, alpha=1.0, zorder=0)
 
-
-# Get get true or false on pairwise correlation
-listOfSets = [set(subset.split(';')) for subset in corumPPI['subunits(Gene name)']]
-pairwiseCorrData = utils.addGroundTruth(
-    listOfSets, pairwiseCorrData, 'Corum', filename='BaseModelPairwise')
-
-
-
-# find the best count threshold n
-best = True
-previousAUC = 0
-threshold = 2
-aucList=[]
-thresholdList =[]
-while threshold < 101:
+#     plt.savefig("thresholdVSAUC.png",
+#                 bbox_inches="tight")
     
-    queriedPairCorrData = pairwiseCorrData.query('counts > @threshold')
-    corrCumSum = np.cumsum(queriedPairCorrData['Corum']) / np.sum(queriedPairCorrData['Corum'])
-    indexes = np.array(queriedPairCorrData.reset_index().index) / queriedPairCorrData.shape[0]
-    currentAUC = auc(indexes, corrCumSum)
-    #Update Lists for Curve plot
-    aucList.append(currentAUC)
-    thresholdList.append(threshold)
+
+# # getAUCvsThresholdPlot(pairwiseCorrData)
+
+#     # What Professor Pedro Asked For :)
+# def randomSubSamplingAUC(proteinsData: pd.DataFrame):
+#     proteinsData :pd.DataFrame = proteinsData.copy()
+
+#     for sampleNum in [5]:
+#         proteinsData = proteinsData.sample(n = sampleNum, axis=0, random_state=RANDOMSTATE)
+#     print(proteinsData)
+#     proteinsData = utils.getPairwiseCorrelation(proteinsData,None,'correlation',False)
+#     print(proteinsData)
     
-    
-    if currentAUC < previousAUC:
-        best = False
-        print('The limit threshold of interactions were we start to lose information after increasing it is: ' + str(threshold-1) + '\n with an auc of ' + str(previousAUC))
-    # print(str(currentAUC) + '>=' + str(previousAUC) + '\n' + str(best))
-    threshold += 1
-    previousAUC = currentAUC
+# randomSubSamplingAUC(proteinsData)
 
-
-# make chart with auc per n threshold
-
-_, ax = plt.subplots(1, 1, figsize=(4, 3), dpi=600)
-ax.plot(
-    thresholdList,
-    aucList,
-    label="BaseLine Model AUC with Various thresholds n",
-    c='green',
-)
-
-ax.plot([2, 100], [0, 1], "k--", lw=0.3)
-ax.legend(loc="lower right", frameon=False)
-
-ax.set_ylabel("Area Under the Curve")
-ax.set_xlabel("Minimum interaction threshold")
-ax.grid(True, axis="both", ls="-", lw=0.1, alpha=1.0, zorder=0)
-
-plt.savefig("thresholdVSAUC.png",
-            bbox_inches="tight")
-
+biogrid = ppiDataset(PATH + '/externalDatasets/biogridPPIReduced.csv.gz',['proteinA', 'proteinB'])
