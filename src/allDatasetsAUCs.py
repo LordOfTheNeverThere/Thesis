d374234c654c9576f4dd3b0333cc2dc981f2cab2
Miguel Dinis de Sousa <miguel.dinis.s@tecnico.ulisboa.pt>
2023-03-30 10:16:27 +0100

    auc preliminary pipeline

--- src/allDatasetsAUCs.py
+++ src/allDatasetsAUCs.py
@@ -18,44 +18,47 @@ pairwiseCorrData = pd.read_csv(
 # #Corum
 # corumPPI = pd.read_json(PATH + '/externalDatasets/corumPPI.json')
 # #String
-# stringPPI = pd.read_json(path_or_buf=PATH + '/externalDatasets/stringPPI.json.gz', compression='gzip')
+stringPPI = pd.read_csv(PATH + '/externalDatasets/stringPPI900Selected.csv.gz', compression='gzip')
 #biogrid
 ppiBiogridFile = open('ppiBiogridTreeNode', 'rb')
 biogridPPI = pickle.load(ppiBiogridFile)
 ppiBiogridFile.close()
 
+stringPPI.drop(columns=['String'],inplace=True)
+
+
+PPIs = TreeNode('root')
+for PPI in stringPPI.to_numpy():
+    childrenValues = PPIs.getChildrenValue()
+
+    if (PPI[0] in childrenValues) or (PPI[1] in childrenValues):
+
+        if PPI[0] in childrenValues and PPI[1] not in childrenValues:
+            proteinA = PPIs.getNodeFirstLayer(PPI[0])
+
+            if PPI[1] not in proteinA.getChildrenValue():
+                proteinA.addChild(TreeNode(PPI[1]))
+
+        elif PPI[1] in childrenValues and PPI[0] not in childrenValues:
+
+            proteinA = PPIs.getNodeFirstLayer(PPI[1])
+
+            if PPI[0] not in proteinA.getChildrenValue():
+
+                proteinA.addChild(TreeNode(PPI[0]))
+
+    else:
+        PPIs.addChild(TreeNode(PPI[0],{TreeNode(PPI[1])}))
+
+
+pairwiseCorrData = utils.addGroundTruthTreeNode(
+    ppiTree=biogridPPI, data=pairwiseCorrData, externalDatasetName="biogrid", filename='BaseModelPairwise')  # 150 sec
+
+ppiStringFile = open('ppiStringTreeNode', 'ab')
+pickle.dump(PPIs,ppiStringFile)
+ppiStringFile.close()
 
 
 
-
-# listOfSets = list(map(set, biogridPPI.to_numpy()))
-
-# PPIs = TreeNode('root')
-# for PPI in biogridPPI.to_numpy():
-#     childrenValues = PPIs.getChildrenValue()
-
-#     if (PPI[0] in childrenValues) or (PPI[1] in childrenValues):
-
-#         if PPI[0] in childrenValues and PPI[1] not in childrenValues:
-#             proteinA = PPIs.getNodeFirstLayer(PPI[0])
-
-#             if PPI[1] not in proteinA.getChildrenValue():
-#                 proteinA.addChild(TreeNode(PPI[1]))
-
-#         elif PPI[1] in childrenValues and PPI[0] not in childrenValues:
-
-#             proteinA = PPIs.getNodeFirstLayer(PPI[1])
-
-#             if PPI[0] not in proteinA.getChildrenValue():
-
-#                 proteinA.addChild(TreeNode(PPI[0]))
-
-#     else:
-#         PPIs.addChild(TreeNode(PPI[0],{TreeNode(PPI[1])}))
-
-
-test = utils.addGroundTruthTreeNode(ppiTree=biogridPPI, data=pairwiseCorrData.head(1000), externalDatasetName="biogrid") ## 150 sec
-
-
-print(test.query('biogrid==1'))            
-
+pairwiseCorrData = utils.addGroundTruthTreeNode(
+    ppiTree=PPIs, data=pairwiseCorrData, externalDatasetName="string", filename='BaseModelPairwise')  # 150 sec
