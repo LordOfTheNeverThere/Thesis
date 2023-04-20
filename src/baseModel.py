dae60792946467ed1d8e9519271288fc78897439
Miguel Dinis de Sousa <miguel.dinis.s@tecnico.ulisboa.pt>
2023-04-20 16:20:13 +0100

    pipeline for pValues of BaseModel

--- src/baseModel.py
+++ src/baseModel.py
@@ -10,9 +10,8 @@ from env import PATH
 if __name__ == '__main__':
 
     proteinsData = ProteinsMatrix(PATH + '/datasetsTese/proteomicsDataTrans.csv', index_col = 'modelID')
-    proteinsData.pearsonCorrelations('baseModePairwiseWithpValues', 'globalCorrelation', counting=False)
-
+    proteinsData.pearsonCorrelations('baseModePairwiseWithpValues', 'globalCorrelation')
+    print(proteinsData.data)
     pairwiseCorrPValues = PairwiseCorrMatrix(PATH + '/datasetsTese/baseModePairwiseWithpValues.csv.gz', compression = 'gzip')
-    pairwiseCorr = PairwiseCorrMatrix(PATH + '/datasetsTese/BaseModelPairwise.csv')
     print(pairwiseCorrPValues.data)
-    print(pairwiseCorr.data.merge(pairwiseCorrPValues.data, on='PPI', how='left'))
+    
