62f4d8f4cec20e8efc5f44330c3b9aa8e0406228
TheLordOfTheNeverThere <miguel.dinis.s@tecnico.ulisboa.pt>
2023-04-26 19:04:49 +0100

    Readptation of the getModelsByQuery Method and query and compare

--- src/classes.py
+++ src/classes.py
@@ -32,6 +32,31 @@ class MatrixData:
             pickle.dump(self,f)
         f.close()
 
+    def query(self, query: str, inplace : bool = False) -> pd.DataFrame|None:
+
+
+        if not inplace:
+            return self.data.query(query)
+        else:
+            self.data = self.data.query(query)
+
+    def compare(self, other: MatrixData, querySelf: str, queryOther: str, key: str = 'PPI') -> pd.DataFrame:
+        """Query two PairwiseCorrMatrices with independent queries and get the merged dataframe as result
+
+        Args:
+            other (PairwiseCorrMatrix): another PairwiseCorrMatrix object
+            querySelf (str): Query to apply on the self object
+            queryOther (str): Query to apply on the other object
+            key (str, optional): In what column name should the merge be happening on, acting as consesual index. Defaults to 'PPI'.
+
+        Returns:
+            pd.DataFrame: _description_
+        """
+
+        left: pd.DataFrame = self.query(querySelf).copy()
+        right: pd.DataFrame = other.query(queryOther).copy()
+
+        return left.merge(right, on=key)
 class ppiDataset(MatrixData):
 
     def __init__(self, filepath:str = None, data: pd.DataFrame = None, proteinLabels: list = [], **readerKwargs):
@@ -237,31 +262,6 @@ class PairwiseCorrMatrix(MatrixData):
         
         self.label = label + f" (AUC {self.auc:.2f})"
 
-    def query(self, query: str, inplace : bool = False) -> pd.DataFrame|None:
-
-
-        if not inplace:
-            return self.data.query(query)
-        else:
-            self.data = self.data.query(query)
-
-    def compare(self, other: PairwiseCorrMatrix, querySelf: str, queryOther: str, key: str = 'PPI') -> pd.DataFrame:
-        """Query two PairwiseCorrMatrices with independent queries and get the merged dataframe as result
-
-        Args:
-            other (PairwiseCorrMatrix): another PairwiseCorrMatrix object
-            querySelf (str): Query to apply on the self object
-            queryOther (str): Query to apply on the other object
-            key (str, optional): In what column name should the merge be happening on, acting as consesual index. Defaults to 'PPI'.
-
-        Returns:
-            pd.DataFrame: _description_
-        """
-
-        left: pd.DataFrame = self.query(querySelf).copy()
-        right: pd.DataFrame = other.query(queryOther).copy()
-
-        return left.merge(right, on=key)
     
 class DrugResponseMatrix(MatrixData):
     """Class interface and methods for the drug response data"""
