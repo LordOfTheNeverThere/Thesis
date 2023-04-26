5117b1b050771b6801c663e0f86ea66676acd73f
TheLordOfTheNeverThere <miguel.dinis.s@tecnico.ulisboa.pt>
2023-04-26 18:54:34 +0100

    drugResponse Binarisation Complete

--- src/classes.py
+++ src/classes.py
@@ -2,6 +2,7 @@ from __future__ import annotations #  postpone evaluation of annotations
 import pandas as pd
 from itertools import combinations
 import numpy as np
+import math
 from sklearn.metrics import auc
 from scipy.stats import pearsonr
 import pickle
@@ -270,7 +271,27 @@ class DrugResponseMatrix(MatrixData):
 
 
     def binrise(self):
-        pass
+        """Creates a Binary representation of the Drug response matrix, where 0 means no efficacy and 1 efficacy. This is done bye calculating a threshold, 50% of the natural log of the [max screen] 
+        """
+        maxScreenConc = pd.read_csv(PATH + '/externalDatasets/drugMaxScreenConcentration.csv', index_col='DRUG_INDEX')
+        maxScreenConc.index.name = 'drug'
+        
+        self.data = self.data.merge(maxScreenConc, on='drug') # We only work with drugs for which we have a max screen concentration and IC50
+        # With 50% of the natural log of the max Screen Concentration we can create an empirically proven threshold, 
+        # where generelly IC50 values greater than that mean that The drug has no efficacy, and below drug has efficancy, 
+        # or in other words the cell line doesn't or does 'respond to a drug'
+        self.data['efficacyThreshold']= self.data['MAX_CONC'].apply(lambda row: math.log(row) * 0.5) 
+        self.data.drop(columns=['MAX_CONC'], inplace=True)
+
+
+        # Create mask that see if all columns are less than the values in the threshold col, and convert them to ints so the bool's become either 0 or 1
+        self.data = self.data.apply(lambda row: row < row['efficacyThreshold'], axis=1).astype(int)
+
+        relevantDrugs = (self.data.sum(axis=1) >= 3) # Condition that only accounts for drugs that kill at leats 3 cell lines
+
+       
+        self.data = self.data.loc[relevantDrugs] # apply condition (We lose 130 drugs, 697 -> 567) 26/4/23
+
 
     
 
