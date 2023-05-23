bad0e08b9a2c013f5222df2ee16382a0212f1f8e
Miguel Dinis de Sousa <miguel.dinis.s@tecnico.ulisboa.pt>
2023-05-23 15:01:29 +0100

    Analysis of scatter plots with various conditions

--- src/draft.py
+++ src/draft.py
@@ -9,33 +9,39 @@ if __name__ == '__main__':
 
 
     corum:ppiDataset = read(PATH + '/externalDatasets/corum.pickle.gz')
-    proteinData :ProteinsMatrix = read(PATH + '/datasetsTese/ogProteomics.pickle.gz')
-    proteinData.data.dropna(axis=1, thresh=round(proteinData.data.shape[0] * 0.2), inplace=True) #We require that a protein has about 20% missingness for it to be considered a dropable column
-    proteinData.data = proteinData.data.fillna(proteinData.data.mean())
-    mvPairwiseCorr : PairwiseCorrMatrix = read(PATH + '/datasetsTese/baseModelFiltered.pickle.gz')
-    pValuePearson = mvPairwiseCorr.data['pValue']
-    glsPairwiseCorr : PairwiseCorrMatrix = read(PATH + '/datasetsTese/glsPairwiseCorr.pickle.gz')
-    glsPairwiseCorr.aucCalculator('corum', 'gls Model', 'p-value', True)
-    pValueGLS = glsPairwiseCorr.data['p-value']
-    pairwiseCorr = proteinData.pearsonCorrelations('pearsonR')
-    pValuePearsonMean = pairwiseCorr.data['pValue']
-    pairwiseCorr.addGroundTruth(ppis=corum.ppis,externalDatasetName='corum')
-    pairwiseCorr.aucCalculator('corum', 'ProteinMean AUC', 'pValue', True)
-    glsPairwiseCorr.write(PATH + '/datasetsTese/glsPairwiseCorr.pickle.gz')
-    pairwiseCorr.write(PATH + '/datasetsTese/baseModelProteinMean.pickle.gz')
-    drawRecallCurves([pairwiseCorr, glsPairwiseCorr],['blue', 'red'], '../images/pValuesOgMeanVsGLSRecallCurve.png')
+    proteinMeanPairwiseCorr:PairwiseCorrMatrix = read(PATH + '/datasetsTese/baseModelProteinMean.pickle.gz')
+    print(proteinMeanPairwiseCorr.query('counts == 949 & corum == 1 & pValue == 0')) #18295 rows
+    print(proteinMeanPairwiseCorr.query('counts == 949 & corum == 0 & pValue == 0')) #4408798 rows
+    # proteinData :ProteinsMatrix = read(PATH + '/datasetsTese/ogProteomics.pickle.gz')
+    # print(proteinData)
+    # proteinData.data.dropna(axis=1, thresh=round(proteinData.data.shape[0] * 0.2), inplace=True) #We require that a protein has about 20% missingness for it to be considered a dropable column
+    # print(proteinData)
+    # proteinData.data = proteinData.data.fillna(proteinData.data.mean())
+    # print(proteinData)
+    # mvPairwiseCorr : PairwiseCorrMatrix = read(PATH + '/datasetsTese/baseModelFiltered.pickle.gz')
+    # pValuePearson = mvPairwiseCorr.data['pValue']
+    # glsPairwiseCorr : PairwiseCorrMatrix = read(PATH + '/datasetsTese/glsPairwiseCorr.pickle.gz')
+    # glsPairwiseCorr.aucCalculator('corum', 'gls Model', 'p-value', True)
+    # pValueGLS = glsPairwiseCorr.data['p-value']
+    # pairwiseCorr = proteinData.pearsonCorrelations('pearsonR')
+    # pValuePearsonMean = pairwiseCorr.data['pValue']
+    # pairwiseCorr.addGroundTruth(ppis=corum.ppis,externalDatasetName='corum')
+    # pairwiseCorr.aucCalculator('corum', 'ProteinMean AUC', 'pValue', True)
+    # glsPairwiseCorr.write(PATH + '/datasetsTese/glsPairwiseCorr.pickle.gz')
+    # pairwiseCorr.write(PATH + '/datasetsTese/baseModelProteinMean.pickle.gz')
+    # drawRecallCurves([pairwiseCorr, glsPairwiseCorr],['blue', 'red'], '../images/pValuesOgMeanVsGLSRecallCurve.png')
 
-    plt.hist(pValuePearson, 30, alpha=0.5, label='p-values R')
-    plt.hist(pValuePearsonMean, 30, alpha=0.5, label='p-values R Mean')
-    plt.hist(pValueGLS, 30, alpha=0.5, label='p-values gls')
+    # plt.hist(pValuePearson, 30, alpha=0.5, label='p-values R')
+    # plt.hist(pValuePearsonMean, 30, alpha=0.5, label='p-values R Mean')
+    # plt.hist(pValueGLS, 30, alpha=0.5, label='p-values gls')
 
-    plt.xlabel('p-value')
-    plt.ylabel('Frequency of p-value')
-    plt.title('Histogram of p-values of different models')
+    # plt.xlabel('p-value')
+    # plt.ylabel('Frequency of p-value')
+    # plt.title('Histogram of p-values of different models')
 
-    plt.legend()
+    # plt.legend()
 
-    plt.savefig('../images/pValuesHist3Models.png')
+    # plt.savefig('../images/pValuesHist3Models.png')
 
 
 
--- src/main.py
+++ src/main.py
@@ -10,6 +10,12 @@ from resources import *
 
 
 if __name__ == '__main__':
+    limitsR = (0.9,1)
+    limitsGLS = (-0.001,0.001)
+    corum = 0
+    lung = False
+    sortingColumn = 'glsCoefficient'
+    sortingType = False
 
 
 
@@ -30,8 +36,8 @@ if __name__ == '__main__':
 
 
 
-    ogPairwise :PairwiseCorrMatrix = read(PATH + '/datasetsTese/baseModelFiltered.pickle.gz')
-    glsPairwise :PairwiseCorrMatrix = read(PATH + '/datasetsTese/glsPairwiseCorr.pickle.gz')
+    vaePairwise :PairwiseCorrMatrix = read(PATH + '/datasetsTese/VAEPearsonPairCorr.pickle.gz')
+    vaeGlsPairwise :PairwiseCorrMatrix = read(PATH + '/datasetsTese/VAEGLSPairCorr.pickle.gz')
     ogProteomics :ProteinsMatrix = read(PATH + '/datasetsTese/ogProteomics.pickle.gz')
     sampleSheet = pd.read_csv(PATH + '/datasetsTese/samplesheet.csv', index_col='model_id')
 
@@ -42,25 +48,33 @@ if __name__ == '__main__':
 
     # Merging/sorting and quering
 
-    mergedData:pd.DataFrame = ogPairwise.data.merge(right = glsPairwise.data[['glsCoefficient']], on ='PPI')
-    mergedData = mergedData.loc[mergedData['corum'] == 0]
-    mergedData = mergedData.loc[mergedData['biogrid'] == 0]
-    mergedData = mergedData.loc[mergedData['string'] == 0]
-    mergedData = mergedData.loc[mergedData['string400'] == 0]
+    mergedData:pd.DataFrame = vaePairwise.data.merge(right = vaeGlsPairwise.data[['glsCoefficient']], on ='PPI')
+    mergedData = mergedData.loc[mergedData['corum'] == corum]
+    # mergedData = mergedData.loc[mergedData['biogrid'] == 0]
+    # mergedData = mergedData.loc[mergedData['string'] == 0]
+    # mergedData = mergedData.loc[mergedData['string400'] == 0]
     
-    mergedData = mergedData.query('globalCorrelation < 0.01 & globalCorrelation > 0').copy().sort_values(['glsCoefficient'], ascending=[False])
+    mergedData = mergedData.query('pearsonR < @limitsR[1] & pearsonR > @limitsR[0] & glsCoefficient > @limitsGLS[0] & glsCoefficient < @limitsGLS[1]').copy().sort_values([sortingColumn], ascending=[sortingType])
+    print(mergedData)
     
     top5evidencePPI = mergedData.head(10)
-    top5evidencePPI.to_csv('../images/top10GlsbetasForHighR.csv')
     indexes = list(top5evidencePPI.index)
     setOfPPIs = [(proteins.split(';')[0], proteins.split(';')[1] ) for proteins in indexes]#Unpack PPIs of opposing inensities into tuples of proteins 
-    fig, ax = plt.subplots(10, 1, figsize=(15, 40))
+    fig, ax = plt.subplots(10, 1, figsize=(20, 70))
     for index, ppi in enumerate(setOfPPIs):
 
         proteinAB =  ogProteomics.data.loc[:,[ppi[0], ppi[1]]] #Get the protein expresion of the two proteins in the ppi
         proteinAB = proteinAB.dropna(axis=0)
+
         samples = list(proteinAB.index) #Get all samples where both proteins have an expression value
+
+        if not lung: # If we dont want a specific tisssue like lung
+
+            samples = [sample for sample in samples if sampleSheet.loc[sample, 'tissue']!='Lung']
+            proteinAB = proteinAB.loc[samples,:]
+        
         tissues = [sampleSheet.loc[sample, 'tissue'] for sample in samples] #get all sample's tissues
+        
 
 
 
@@ -70,11 +84,11 @@ if __name__ == '__main__':
         ax[index].scatter(proteinAB[ppi[0]], proteinAB[ppi[1]], c = tissues)
         ax[index].set_xlabel(ppi[0])
         ax[index].set_ylabel(ppi[1])
-        ax[index].set_title("Protein expression across samples, by Tissue, $r∈(0,0.001]$, $β_{GLS}↑$")
+        ax[index].set_title(f"Protein expression across samples, by Tissue, $r∈{limitsR}$, $β_{{GLS}}∈{limitsGLS}$, corum={corum}, lung={lung}")
         ax[index].tick_params(labelsize=16)
         legend = [plt.Line2D([0], [0], marker='o', color=TISSUEPALETTE[key], label=key) for key in TISSUEPALETTE]
         ax[index].legend(handles = legend, fontsize=8, framealpha=0.2)
 
-    plt.savefig('../images/correlationsPearsonOnlyWithTissueTypev5.0.png')
+    plt.savefig('../images/correlationsPearsonOnlyWithTissueTypev8.1.png')
 
 
