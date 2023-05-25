3ce362da49ac17d12ae9490bc34097b6c1cc59ff
TheLordOfTheNeverThere <miguel.dinis.s@tecnico.ulisboa.pt>
2023-05-25 13:30:30 +0100

    Roll back on class changes

--- src/resources/__init__.py
+++ src/resources/__init__.py
@@ -1,5 +1,5 @@
 from .env import PATH
 from .config import TISSUEPALETTE
-from .classes import MatrixData, ppiDataset, PairwiseCorrMatrix, ProteinsMatrix, DrugResponseMatrix, LinearRegression
+from .classes import MatrixData, ppiDataset, PairwiseCorrMatrix, ProteinsMatrix, DrugResponseMatrix
 from .utils import read, drawRecallCurves, getUniqueSetValues, getModelsByQuery, getPairwiseCorrelation, addGroundTruth
 
