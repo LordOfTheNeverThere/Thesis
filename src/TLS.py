2474ba86d676144edefe7a065559f2d718e20a39
Miguel Dinis de Sousa <miguel.dinis.s@tecnico.ulisboa.pt>
2023-05-29 12:02:47 +0100

    Prior to redoing all auc calculation

--- src/TLS.py
+++ src/TLS.py
@@ -8,8 +8,43 @@ from resources import *
 
 
 proteomics:ProteinsMatrix = read(PATH + '/datasetsTese/ogProteomics.pickle.gz')
-glsPairwise:PairwiseCorrMatrix = read(PATH + '/datasetsTese/glsPairwiseCorr.pickle.gz')
+corum: ppiDataset = read(PATH + '/externalDatasets/corum.pickle.gz')
+pearsonCorrs:PairwiseCorrMatrix = read(PATH + '/datasetsTese/baseModelFiltered.pickle.gz') # R
+vaeGLSCorrs:PairwiseCorrMatrix = read(PATH + '/datasetsTese/VAEGLSPairCorr.pickle.gz') # gls + vae
+vaePearsonCorrs:PairwiseCorrMatrix = read(PATH + '/datasetsTese/VAEPearsonPairCorr.pickle.gz') # R + vae
 
-proteomics.tlsResidues(glsPairwise) # Correct This!
 
+glsCorrs = proteomics.getGLSCorr(coefColumnName='beta')
+glsCorrs.addGroundTruth(corum.ppis, 'corum') # gls
+
+#Add all aucs
+
+glsCorrs.aucCalculator('corum', 'gls','pValue', True)
+glsCorrs.aucCalculator('corum', 'gls','beta', False)
+print(glsCorrs)
+glsCorrs.write(PATH + '/datasetsTese/glsPairwiseCorr.pickle.gz')
+
+pearsonCorrs.aucCalculator('corum', 'pearson','pValue', True)
+pearsonCorrs.aucCalculator('corum', 'pearson','beta', False)
+print(pearsonCorrs)
+pearsonCorrs.write(PATH + '/datasetsTese/baseModelFiltered.pickle.gz')
+
+vaeGLSCorrs.aucCalculator('corum', 'VAE-GLS','pValue', True)
+vaeGLSCorrs.aucCalculator('corum', 'VAE-GLS','beta', False)
+print(vaeGLSCorrs)
+vaeGLSCorrs.write(PATH + '/datasetsTese/VAEGLSPairCorr.pickle.gz')
+
+vaePearsonCorrs.aucCalculator('corum', 'VAE-pearson','pValue', True)
+vaePearsonCorrs.aucCalculator('corum', 'VAE-pearson','beta', False)
+print(vaePearsonCorrs)
+vaePearsonCorrs.write(PATH + '/datasetsTese/VAEPearsonPairCorr.pickle.gz')
+
+
+
+
+
+# tlsResidualsMatrix = proteomics.tlsResidues(glsPairwise)
+# print(glsPairwise.data.loc[glsPairwise.data['p-value'] > 0])
+# print(tlsResidualsMatrix.data.shape)
+# print(tlsResidualsMatrix.data.isna().sum().sum())
 
--- src/glsCorr.py
+++ src/glsCorr.py
@@ -3,23 +3,20 @@ import pandas as pd
 from scipy.special import stdtr
 import matplotlib.pyplot as plt
 import time as t
-from resources import *
+from resources import ProteinsMatrix, PATH, drawRecallCurves, read, ppiDataset
 
 
 if __name__ == '__main__':
 
-    mofa2 = ProteinsMatrix(PATH + '/datasetsTese/proteomicsVAE.csv.gz', compression='gzip', index_col='Unnamed: 0')
-    mofa2.write(PATH + '/datasetsTese/proteomicsVAE.pickle.gz')
+    proteomics: ProteinsMatrix = read(PATH + '/datasetsTese/ogProteomics.pickle.gz')
+
     corum :ppiDataset = read(PATH + '/externalDatasets/corum.pickle.gz')
     
-    pearsonPairwiseCorrs = mofa2.pearsonCorrelations('pearsonR') # get gls coefs and pearson r's for the inputed matrix
-    glsPairwiseCorrs = mofa2.getGLSCorr()
+    glsPairwiseCorrs = proteomics.getGLSCorr()
 
-    pearsonPairwiseCorrs.addGroundTruth(corum.ppis,'corum') #Add Ground truths and calculate AUC
-    pearsonPairwiseCorrs.aucCalculator('corum', 'VAE-pearson AUC')
 
     glsPairwiseCorrs.addGroundTruth(corum.ppis,'corum') #Add Ground truths and calculate AUC
-    glsPairwiseCorrs.aucCalculator('corum', 'VAE-gls AUC')
+    glsPairwiseCorrs.aucCalculator('corum', 'gls')
 
     pearsonPairwiseCorrs.write(PATH + '/datasetsTese/VAEPearsonPairCorr.pickle.gz')
     glsPairwiseCorrs.write(PATH + '/datasetsTese/VAEGLSPairCorr.pickle.gz')
