698b7855ab2babd5ee69adfd1fae7b7996b2e2f4
Miguel Dinis de Sousa <miguel.dinis.s@tecnico.ulisboa.pt>
2023-06-19 00:02:17 +0100

    Playing with Shappiro, BrueshPagan and White tests

--- src/draft.py
+++ src/draft.py
@@ -3,16 +3,38 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from resources import *
 import numpy as np
+from sklearn.linear_model import LinearRegression
+from scipy.stats import shapiro, pearsonr
+from statsmodels.stats.diagnostic import het_white, het_breuschpagan
 
 
 if __name__ == '__main__':
 
 
 
+    proteomics: pd.DataFrame = read(PATH + '/internal/proteomics/ogProteomics.pickle.gz').data
+    x = proteomics.iloc[:,10].dropna()
+    y = proteomics.iloc[:,15].dropna()
+    samplesCommon = x.index.intersection(y.index)
+    x = x.loc[samplesCommon]
+    X = pd.DataFrame({'x':x, 'intercept':np.ones(len(x))})
+    y = y.loc[samplesCommon]
+    pearsonr(x,y)
 
+    regressor = LinearRegression()
+    regressor.fit(x.values.reshape(-1,1), y.values.reshape(-1,1)) 
+    regressor.score(x.values.reshape(-1,1), y.values.reshape(-1,1))
 
+    residuals = y.values.reshape(-1,1) - regressor.predict(x.values.reshape(-1,1))
+    het_breuschpagan(residuals, X)
+    het_white(residuals, X)
 
-
+    plt.close()
+    plt.scatter(x, y)
+    plt.show()
+    plt.close()
+    plt.scatter(x, residuals)
+    plt.show()
     
 
 
